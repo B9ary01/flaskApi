828ec5a3d71ae57b163b8543dda329ed2f4c6ab7
B9ary01 <bishalrai111@yahoo.com>
2022-09-13 13:45:36 +0100

    add update, delete, show routes

--- app.py
+++ app.py
@@ -30,5 +30,16 @@ def city():
     resp , code=fns[request.method](request)
     return jsonify(resp), code
 
+@app.route('/cities/<int:city_id>',methods=['GET','PUT', 'DELETE'])
+def cat_handler(city_id):
+    fns={
+        'GET':cities.show,
+        'PUT':cities.update,
+        'DELETE':cities.destroy
+       
+    }
+    resp , code=fns[request.method](request, city_id)
+    return jsonify(resp), code
+
 if __name__ == '__main__':
    app.run(debug=True)
--- controllers/cities.py
+++ controllers/cities.py
@@ -1,3 +1,4 @@
+from logging import exception
 
 cities=[
     { 'id':1, 'name':'Namche Bazar','population':23000},
@@ -14,3 +15,29 @@ def create(req):
     new_city['id']=sorted([c['id'] for c in cities])[-1]+1
     cities.append(new_city)
     return new_city,201
+
+
+def show(req,uid):
+    return find_by_uid(uid), 200
+
+
+def update(req, uid):
+    city=find_by_uid(uid)
+    data=req.get_json()
+    for key, val in data.items():
+        city[key]=val
+    return city, 200
+
+def destroy(req,uid):
+    city=find_by_uid(uid)
+    cities.remove(city)
+    return city, 204
+
+
+
+
+def find_by_uid(uid):
+    try:
+        return next(c for c in cities if c['id']==uid)
+    except:
+        raise Exception("we do not have this cat with this {uid}")
