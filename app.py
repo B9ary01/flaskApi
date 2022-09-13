4277baf87eb52c9b473bb863fa855a120184126d
B9ary01 <bishalrai111@yahoo.com>
2022-09-13 11:47:10 +0100

    add GET, POST route

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from  werkzeug import exceptions
 
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
     return "welcome to home page."
+
+@app.route('/cities',methods=['GET','POST'])
+def city():
+    if request.method=='GET' :
+        return jsonify({'cities':['Dhatan','Chitwan','Kathmandu']}),200
+
+    elif request.method=='POST':
+        return "we have posted some cities",201
 
 
 if __name__ == '__main__':
